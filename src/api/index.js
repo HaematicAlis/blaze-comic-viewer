4d804d1f681678d332e24799ae0ad68b9a84a177
Blaze Wiseman <kawaiimicrowaves930@gmail.com>
2021-07-05 15:27:56 -0400

    updates

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://10.0.0.14:5000';
+//const url = 'http://10.0.0.14:5000';
+const url = 'https://blaze-comic-viewer.herokuapp.com/';
 
 export const register = (registerInfo) => axios.post(url + '/account/register', registerInfo);
 export const login = (loginInfo) => axios.post(url + '/account/login', loginInfo);
