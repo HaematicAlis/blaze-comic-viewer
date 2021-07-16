61279230f9f986dd17ae53bb6ce8fa3f36ddc7f4
Blaze Wiseman <kawaiimicrowaves930@gmail.com>
2021-07-16 15:04:42 -0400

    Comic name and imgur link reset on successful upload

--- src/components/Shelf/Preview/Preview.js
+++ src/components/Shelf/Preview/Preview.js
@@ -9,6 +9,7 @@ import useStyles from './styles.js';
 const Preview = () => {
     const [comicName, setComicName] = useState('');
     const [imgurAlbum, setImgurAlbum] = useState('');
+    const [imgurError, setImgurError] = useState('');
     const session = useSelector((state) => state.session);
     const classes = useStyles();
     const dispatch = useDispatch();
@@ -16,10 +17,10 @@ const Preview = () => {
     const doUpload = () => {
         let name, album;
         comicName ? name = comicName : name = 'Untitled';
-        imgurAlbum ? album = imgurAlbum : album = 'none';
 
         const comic = { name: name, owner: session.id, album: album };
-        setImgurAlbum('');
+        document.getElementById('imgurField').value = '';
+        document.getElementById('nameField').value = '';
         dispatch(addComic(comic));
     }
 
@@ -30,14 +31,18 @@ const Preview = () => {
                     <Paper className={classes.previewPaper}>
                         <Grid container spacing={2} direction="column" align="center" justify="center">
                             <Grid item>
-                                <Typography variant="body1">You are logged in as <span style={{color: 'red'}}>{session.username}.</span></Typography>
+                                <Typography variant="body1">You are logged in as <span style={{color: 'red'}}>{session.username}</span>.</Typography>
                                 <Typography variant="body1">Import a comic to get started.</Typography>
                             </Grid>
                             <Grid item>
-                                <TextField variant="outlined" label="Imgur album hash" color="secondary" size="small" onChange={(e) => setImgurAlbum(e.target.value)}></TextField>
+                                <TextField id="imgurField" variant="outlined" label="Imgur album hash" color="secondary" size="small"
+                                    onChange={(e) => setImgurAlbum(e.target.value)}
+                                    error={imgurError ? true : false}
+                                    helperText={imgurError}>
+                                </TextField>
                             </Grid>
                             <Grid item>
-                                <TextField variant="outlined" label="Give your comic a name" color="secondary" size="small" onChange={(e) => setComicName(e.target.value)}></TextField>
+                                <TextField id="nameField" variant="outlined" label="Give your comic a name" color="secondary" size="small" onChange={(e) => setComicName(e.target.value)}></TextField>
                             </Grid>
                             <Grid item>
                                 <Button disabled={imgurAlbum ? false : true} variant="outlined" color="secondary" onClick={doUpload}>Upload</Button>
